e91063a9a3400330db68ec524c3ad1783445c7a1
pythonhacker <pythonhacker>
2005-08-10 14:33:35 +0000

    *** empty log message ***

--- HarvestMan/HarvestMan/install.py
+++
@@ -1,9 +0,0 @@
-# py2exe install script for harvestman
-
-from distutils.core import setup
-
-import py2exe
-
-setup(name="HarvestMan",
-      scripts=["harvestman.py"],
-)
--- HarvestMan/HarvestMan/py2exesetup.bat
+++
@@ -1,3 +0,0 @@
-rem Batch file for creating py2exe executable for Harvestman
-@echo off
-python install.py py2exe -O2 --packages=encodings --force-imports encodings
