e7173eceaa854540760a30cb049210274cc22afa
pythonhacker <pythonhacker>
2005-09-09 11:21:26 +0000

    Bug-fixes for py2exe version 0.6.1

--- HarvestMan/HarvestMan/py2exesetup.bat
+++ HarvestMan/HarvestMan/py2exesetup.bat
@@ -1,3 +1,5 @@
 rem Batch file for creating py2exe executable for Harvestman
+rem Note that this requires the latest version of py2exe i.e,
+rem 0.6.1 upwards.
 @echo off
-python install.py py2exe -O2 --packages=encodings --force-import encodings
+python install.py py2exe -O2 --bundle 
