819872c2253d7496b594b540cb15d40e57f89a72
dinkoh <dinkoh>
2005-11-02 20:49:59 +0000

    creating some load-tests. the code is not consistent right now.

--- HarvestMan/D-HarvestMan/source/simpleslave.py
+++ HarvestMan/D-HarvestMan/source/simpleslave.py
@@ -3,39 +3,52 @@ import timeit
 
 class SimpleSlave:
     manager = None
-    manager_uri = "PYROLOC://193.217.3.94:7766/manager"
+    manager_uri = "PYROLOC://localhost:7766/manager"
     
     def __init__(self):
        self.mproxy = Pyro.core.getProxyForURI(self.manager_uri)
        #pass
+        
+    def threadCode(self):
+        slave = SimpleSlave() 
+        for i in range(1,2):        
+            domain = slave.mproxy.fetch_url()
+            #print "fetched domain: " + domain
+            url = "http://www.hia.no/" +str()+threading.currentThread().getName()
+            t = timeit.Timer("slave.mproxy.url_found('"+str(url)+"')", "from threadCode import slave")
+            res = t.repeat(1,1)
+            print res
+            
+            slave.mproxy.url_found()
+            #slave.mproxy.url_found("http://www.ffi.no"
+            time.sleep(0.5)
+            slave.mproxy.url_found("http://www.hia.no/" +str(i)+threading.currentThread().getName())
+            
+            #slave.mproxy.heartbeat(domain)
+            #slave.mproxy.crawl_failed(domain)
+        
+        slave.mproxy.crawl_finished(domain)    
 
-def threadCode():
-    slave = SimpleSlave() 
-    for i in range(1,61):        
-        domain = slave.mproxy.fetch_url()
-        #print "fetched domain: " + domain
-        url = "http://www.hia.no/" +str(i)+threading.currentThread().getName()
-        t = timeit.Timer("test("+str(url)+")", "")
-        res = t.repeat(1,1)
-        print res
-        
-        slave.mproxy.url_found()
-        #slave.mproxy.url_found("http://www.ffi.no"
-        time.sleep(0.5)
-        slave.mproxy.url_found("http://www.hia.no/" +str(i)+threading.currentThread().getName())
-        
-        #slave.mproxy.heartbeat(domain)
-        #slave.mproxy.crawl_failed(domain)
-    
-    slave.mproxy.crawl_finished(domain)    
+def test():
+    #slave = SimpleSlave()
+    #for i in range(1,2):
+    slave.mproxy.url_found("http://www.hia.no/" +str(1)+threading.currentThread().getName())
+        #time.sleep(0.5)
+        #slave.mproxy.url_found("http://www.hia.no/" +str(i)+threading.currentThread().getName())
+        #time.sleep(0.5)
 
 if __name__== '__main__':
     print "Starting SimpleSlave..."
-    slave = SimpleSlave()
+    
     for i in range(0,100):
-        t = threading.Thread(target = threadCode, name = "child" + str(i))
+        slave = SimpleSlave()
+        t = threading.Thread(target = slave.threadCode(), name = "child" + str(i))
         t.start()
 
+    #t = timeit.Timer("test()", "from __main__ import test")
+    #res = t.repeat(100, 1)
+    #for i in res:
+    #    print i
 
     
     
