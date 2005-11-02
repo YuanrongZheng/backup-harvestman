4b431b839dc8917b3eae69b2f4ecd609dc26da26
dinkoh <dinkoh>
2005-11-02 20:11:34 +0000

    coding some load tests....

--- HarvestMan/D-HarvestMan/source/simpleslave.py
+++ HarvestMan/D-HarvestMan/source/simpleslave.py
@@ -1,30 +1,39 @@
 import Pyro.core,threading,time
+import timeit
 
 class SimpleSlave:
     manager = None
-    manager_uri = "PYROLOC://localhost:7766/manager"
+    manager_uri = "PYROLOC://193.217.3.94:7766/manager"
     
     def __init__(self):
        self.mproxy = Pyro.core.getProxyForURI(self.manager_uri)
        #pass
 
-def threadCode():         
-    for i in range(1,10):
-        slave = SimpleSlave()
+def threadCode():
+    slave = SimpleSlave() 
+    for i in range(1,61):        
         domain = slave.mproxy.fetch_url()
         #print "fetched domain: " + domain
-        slave.mproxy.url_found("http://www.hia.no/" +str(i)+threading.currentThread().getName())
+        url = "http://www.hia.no/" +str(i)+threading.currentThread().getName()
+        t = timeit.Timer("test("+str(url)+")", "")
+        res = t.repeat(1,1)
+        print res
+        
+        slave.mproxy.url_found()
         #slave.mproxy.url_found("http://www.ffi.no"
-        slave.mproxy.heartbeat(domain)
-        time.sleep(1)
-        slave.mproxy.crawl_finished(domain)
-        slave.mproxy.crawl_failed(domain)
+        time.sleep(0.5)
+        slave.mproxy.url_found("http://www.hia.no/" +str(i)+threading.currentThread().getName())
+        
+        #slave.mproxy.heartbeat(domain)
+        #slave.mproxy.crawl_failed(domain)
+    
+    slave.mproxy.crawl_finished(domain)    
 
-if __name__=='__main__':
+if __name__== '__main__':
     print "Starting SimpleSlave..."
     slave = SimpleSlave()
-    for i in range(0,5):
-        t = threading.Thread(target = threadCode, name = "child"+str(i))
+    for i in range(0,100):
+        t = threading.Thread(target = threadCode, name = "child" + str(i))
         t.start()
 
 
