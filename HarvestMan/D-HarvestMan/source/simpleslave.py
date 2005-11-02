3236cfb9aaae2eb9b4750a7af3dfd677e1afb9dc
aandas99 <aandas99>
2005-11-02 19:04:14 +0000

    added threading in simpleslave

--- HarvestMan/D-HarvestMan/source/simpleslave.py
+++ HarvestMan/D-HarvestMan/source/simpleslave.py
@@ -1,21 +1,32 @@
-import Pyro.core
+import Pyro.core,threading,time
 
 class SimpleSlave:
     manager = None
     manager_uri = "PYROLOC://localhost:7766/manager"
     
     def __init__(self):
-        self.mproxy = Pyro.core.getProxyForURI(self.manager_uri)
+       self.mproxy = Pyro.core.getProxyForURI(self.manager_uri)
+       #pass
+
+def threadCode():         
+    for i in range(1,10):
+        slave = SimpleSlave()
+        domain = slave.mproxy.fetch_url()
+        #print "fetched domain: " + domain
+        slave.mproxy.url_found("http://www.hia.no/" +str(i)+threading.currentThread().getName())
+        #slave.mproxy.url_found("http://www.ffi.no"
+        slave.mproxy.heartbeat(domain)
+        time.sleep(1)
+        slave.mproxy.crawl_finished(domain)
+        slave.mproxy.crawl_failed(domain)
 
 if __name__=='__main__':
     print "Starting SimpleSlave..."
     slave = SimpleSlave()
-    domain = slave.mproxy.fetch_url()
-    print "fetched domain: " + domain
-    slave.mproxy.url_found("http://www.hia.no")
-    slave.mproxy.url_found("http://www.ffi.no")
-    slave.mproxy.heartbeat(domain)
-    slave.mproxy.crawl_finished(domain)
-    slave.mproxy.crawl_failed(domain)
+    for i in range(0,5):
+        t = threading.Thread(target = threadCode, name = "child"+str(i))
+        t.start()
+
+
     
     
