37e4472edd807c7dc340cf514cbcdbe77c193b83
pythonhacker <pythonhacker>
2005-11-13 16:11:49 +0000

    *** empty log message ***

---
+++ HarvestMan/D-HarvestMan/source/constants.py
@@ -0,0 +1,32 @@
+"""
+D-HarvestMan constants.
+
+Created  - Anand B Pillai 07/11/05
+
+"""
+
+# Slave status
+
+# Slave status unknown
+SLAVE_STATUS_UNKNOWN = -1
+# Slave is not running locally
+SLAVE_NOT_RUNNING = 0
+# Slave is running
+SLAVE_RUNNING = 1
+# Slave is running, but is suspended
+# currently
+SLAVE_SUSPENDED = 2
+# Slave is stopped by Manager
+SLAVE_STOPPED = 3
+# Slave not responding
+SLAVE_NOT_RESPONDING = 4
+
+
+# Crawl status
+CRAWL_STARTED = 10
+CRAWL_FINISHED = 11
+
+
+
+
+
