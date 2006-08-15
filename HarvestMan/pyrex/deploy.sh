80de9360c38f0e4aaac0279ec2ffdb8df21fba7c
pythonhacker <pythonhacker>
2006-08-15 09:16:59 +0000

    Committing deploy script for pyrex

--- HarvestMan/pyrex/deploy.sh
+++ HarvestMan/pyrex/deploy.sh
@@ -44,8 +44,10 @@ function backup_py_files()
 {
     for i in *.pyx
     do
-      fname = `froot $i`
-      mv $src/$fname$extn $src/$fname$extn$old
+      fname=`froot $i`
+      source=$src/$fname$extn
+      echo "Backing up $source..."
+      mv $source $src/$fname$extn$old
       rm -rf $src$extn*
     done
 }
@@ -56,12 +58,84 @@ function copy_extensions()
     do
       fname=`froot $i`
       sofile=$fname$soextn
+      echo "Copying extension $sofile..."
       cp $sofile $src
     done
 }
-    
-build
-backup_py_files
-copy_extensions
-echo "Done."
+
+function remove_extensions()
+{
+   for i in *.pyx
+    do
+      fname=`froot $i`
+      sofile=$fname$soextn
+      if [ -f $src/$sofile ]; 
+          then
+          echo "Removing extension $src/$sofile..."
+          rm -rf $src/$sofile
+          fi
+          
+    done
+
+}
+
+function restore_py_files()
+{
+    for i in *.pyx
+      do
+      fname=`froot $i`
+      oldpy=$src/$fname$extn$old
+      if [ -f $oldpy ]
+          then
+          echo "Restoring file $src/$fname$extn..."
+          cp $oldpy $src/$fname$extn
+          rm -rf $oldpy
+          fi
+      done
+}
+
+function clean_up()
+{
+    echo "Cleaning up cwd..."
+    rm -rf *.so
+    rm -rf *.c
+}
+
+function deploy()
+{    
+    build
+    backup_py_files
+    copy_extensions
+    echo "Done."
+}
+
+function undeploy()
+{
+    remove_extensions
+    restore_py_files
+    clean_up
+    echo "Done."
+}
+
+function usage()
+{
+    echo "Usage: $0 options"
+    echo
+    echo "[options]"
+    echo "    --deploy      Build and deploy Pyrex extensions"
+    echo "    --undeploy    Remove and undeploy Pyrex extensions"
+}
+
+function dowork()
+{
+    case $1 in
+        "--deploy") deploy;;
+        "--undeploy") undeploy;;
+    esac
+}
+
+case $# in
+0) usage; exit 2;;
+1) dowork $1;;
+esac
 
