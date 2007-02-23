38e11d06722226deab3594b49c9603c23ce036c6
vaikings <vaikings>
2007-02-23 17:45:52 +0000

    refactored to allow for adding more dependency checks

--- HarvestMan-1.5/check_dep.py
+++ HarvestMan-1.5/check_dep.py
@@ -3,37 +3,49 @@
 
 # Dependency checker version 1
 # Author: Anand B Pillai
+#
+# This tool is used to check dependencies for Harvestman 
+#
+
+welcome_message =  """
+x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x
+* Welcome to %s dependency checker*
+x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x\n"""
+
+import sys
+
+def check_pyversion():
+    """check dependency with python version used 
+    """
+    err = 0
+    py = sys.executable
+    
+    if sys.version_info < (2,2):
+        err = 1
+        print 'Dependency failed: You need %s with version 2.2 or higher to run %s.' % (py, app)
+    elif sys.version_info[:2] == (2,2):
+        err = 2
+        print '%s with version 2.2 detected, the following features will be disabled:'
+        print '\t1.Tar Archival feature'
+    else:
+        version = "%d.%d.%d"%sys.version_info[:3]
+        print '%s with version %s detected' % (py, version)
+
+    return err 
 
 if __name__=="__main__":
-    import sys
-
-    app = 'HarvestMan'
-    py = 'Python'
-
-    err=0
     
-    print 'x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x'
-    print '* Welcome to %s dependency checker*' % app
-    print 'x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x-x\n'
+    if len(sys.argv) > 1 :
+        app = sys.argv[1]
+    else:
+        app = 'HarvestMan'
 
-    ver = sys.version_info
-    
-    if (ver[0] == 2 and ver[1] < 2) or (ver[0] < 2):
-        err=1
-        print 'Dependency failed: You need %s 2.2 or higher to run %s.' % (py, app)
-    elif (ver[0] == 2 and ver[1] == 2):
-        err=2
-        print '%s version 2.2 detected, the following features will be disabled:'
-        print '\t1.Tar Archival feature'
-    elif (ver[0] == 2 and ver[1] == 3):
-        print '%s 2.3 version detected' % py
-    elif (ver[0] == 2 and ver[1] == 4):
-        print '%s 2.4 version detected' % py        
+    print welcome_message %app
 
-    if err==1:
-        print '%s will not run' % app
-    elif err==2:
-        print '%s will run, but with limited features' % app
-    elif err==0:
-        print '%s will run without problems.' % app
+    err = check_pyversion() 
 
+    errormap = { 1: "%s will not run",
+                 2: "%s will run but with limited features",
+                 0: "%s will run without problems"}
+
+    print errormap[err]%app
