ae4fcf82c13b08155bdab233c3cb200cd05c0cdd
pythonhacker <pythonhacker>
2007-04-04 12:41:44 +0000

    Modifying to work with URLs

--- HarvestMan-1.5/HarvestMan/dev/feedparser.py
+++ HarvestMan-1.5/HarvestMan/dev/feedparser.py
@@ -1,32 +1,28 @@
 # -- coding: latin-1
-# Not used yet in HarvestMan
+""" A simple RSS feed parser returning a list of links
+in a given feed.
+
+Created Anand B Pillai <abpillai@gmail.com> xx-xx-xxxx
+Modified Anand B Pillai <abpillai@gmail.com> Apr 4 2007 Fixed stuff, added
+                        function to get feeds from URL.
+
+Copyright(C) 2007  Anand B Pillai
+
+"""
+
 
 import xml.parsers.expat
+import urllib2
 
-class ConfigParser(object):
+class FeedParser(object):
 
-    def __init__(self, stream):
+    def __init__(self):
         self._node = ''
-        self._stream = stream
-       
-    def close(self):
-        try:
-            self._stream.close()
-        except Exception, e:
-            print e
- 
+        self.links = []
+        
     def start_element(self, name, attrs):
        
-        if attrs:
-            # If the element has attributes
-            # it does not have CDATA. So set
-            # curr elem to null.
-            self._node = ''
-            
-            for key, value in attrs.iteritems():
-                # Form key name in xml map
-                xmlkey = "".join((name, "_", key))
-        else:
+        if not attrs:
             # If element has no attributes, the
             # value will be in CDATA. Store the
             # element name so that we can use it
@@ -38,24 +34,39 @@ class ConfigParser(object):
         # start element is called. If the
         # element is of interest, set it's
         # option.
-        # data = data.strip()
         
         if self._node and data:
             # print 'Setting option for %s %s ' % (self._node, data)
             if self._node=='link':
-                print data
-                self._stream.write(data)
+                data = data.strip()
+                if data:
+                    try:
+                        self.links.index(data)
+                    except ValueError:
+                        self.links.append(data)
 
-if __name__=="__main__":
-    p = xml.parsers.expat.ParserCreate()
-    c = ConfigParser(open('links.txt','w'))
-    
-    p.StartElementHandler = c.start_element
-    p.CharacterDataHandler = c.char_data
+def getFeedUrls(rssurl):
+    """ Return feed URLs from the given rss URL """
 
     try:
-        p.Parse(open('cnet-rss.xml').read())
-        c.close()
-    except xml.parsers.expat.ExpatError, e:
+        f = urllib2.urlopen(rssurl)
+        data = f.read()
+        p = xml.parsers.expat.ParserCreate()
+        c = FeedParser()
+    
+        p.StartElementHandler = c.start_element
+        p.CharacterDataHandler = c.char_data
+
+        try:
+            p.Parse(data)
+        except xml.parsers.expat.ExpatError, e:
+            print e
+
+        return c.links
+    except Exception, e:
         print e
         
+    
+if __name__=="__main__":
+    print getFeedUrls('http://osnews.com/files/recent.xml')
+    print getFeedUrls('http://www.technologyreview.com/rss/rss.aspx?z=316')    
